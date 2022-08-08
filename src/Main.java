a56af0731ac183c22dbb73761403764134823d2b
Евгений <grekovqwe@gmail.com>
2022-08-08 11:40:55 +0500

    1.6 Массивы ч.2

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,71 @@
 public class Main {
     public static void main(String[] args) {
 
-        // 1.5 Массивы ч.1
-        System.out.println ();
-        System.out.println ("     1.5. Массивы ч.1");
-        // Это не удалять, а исправить.
+        // 1.6 Массивы ч.2
+
+        int[] arr = generateRandomArray();
+        // Проверить массив создался ли
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.println (arr[i] +", ");
+        }
+        System.out.println (arr[arr.length - 1]);
 
         // Задание 1
         System.out.println ();
         System.out.println ("     Задание 1");
-
-        int[] arrayInt = new int[3];
-        arrayInt[0] = 1;
-        arrayInt[1] = 2;
-        arrayInt[2] = 3;
-
-        double[] arrayDouble = {1.57, 7.654, 9.986};
-
-        boolean[] arrayBoolean = {true, false, false};
-
+        int summa = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summa = summa + arr[i];
+        }
+        System.out.println ("Сумма трат за месяц составила " + summa + " рублей");
         System.out.println ("   Готово");
+
 
         // Задание 2
         System.out.println ();
         System.out.println ("     Задание 2");
-        for (int i = 0; i < arrayInt.length - 1; i++) {
-            System.out.print (arrayInt[i] +", ");
+        int minR = 200000;
+        int maxR = 100000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= minR) minR = arr[i];
         }
-        System.out.println (arrayInt[arrayInt.length - 1]);
-        for (int i = 0; i < arrayDouble.length - 1; i++) {
-            System.out.print (arrayDouble[i] +", ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= maxR) maxR = arr[i];
         }
-        System.out.println (arrayDouble[arrayDouble.length - 1]);
-        for (int i = 0; i < arrayBoolean.length - 1; i++) {
-            System.out.print (arrayBoolean[i] +", ");
-        }
-        System.out.println (arrayBoolean[arrayBoolean.length - 1]);
-
+        System.out.println ("Минимальная сумма трат за день составила " + minR + " рублей.");
+        System.out.println ("Максимальная сумма трат за день составила " + maxR + " рублей.");
         System.out.println ("   Готово");
 
         // Задание 3
         System.out.println ();
         System.out.println ("     Задание 3");
-        for (int i = arrayInt.length - 1; i > 0; i--) {
-            System.out.print (arrayInt[i] +", ");
-        }
-        System.out.println (arrayInt[0]);
-        for (int i = arrayDouble.length - 1; i > 0; i--) {
-            System.out.print (arrayDouble[i] +", ");
-        }
-        System.out.println (arrayDouble[0]);
-        for (int i = arrayBoolean.length - 1; i > 0; i--) {
-            System.out.print (arrayBoolean[i] +", ");
-        }
-        System.out.println (arrayBoolean[0]);
+        double middle = summa / 30d;
+        System.out.println ("Средняя сумма трат за месяц составила " + middle + " рублей.");
         System.out.println ("   Готово");
 
         // Задание 4
         System.out.println ();
         System.out.println ("     Задание 4");
 
-        for (int i = 0; i < arrayInt.length - 1; i++) {
-            if (arrayInt[i] % 2 != 0) arrayInt[i]++;
-            System.out.print (arrayInt[i] +", ");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print (reverseFullName[i]);
         }
-        if (arrayInt[arrayInt.length - 1] % 2 != 0) arrayInt[arrayInt.length - 1]++;
-        System.out.println (arrayInt[arrayInt.length - 1]);
-
         System.out.println ();
-        System.out.println ("...");
+        System.out.println ("   Готово");
 
-        // Задание 5
-//        System.out.println ();
-//        System.out.println ("     Задание 5");
-//        System.out.println ();
-//        System.out.println ("...");
 
-        // Задание 6
-//        System.out.println ();
-//        System.out.println ("     Задание 6");
-//        System.out.println ();
-//        System.out.println ("...");
+    }
 
+
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
