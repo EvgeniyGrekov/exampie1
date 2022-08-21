e5dd790238732ecba524f2467f3453797230b9df
Евгений <grekovqwe@gmail.com>
2022-08-22 02:02:24 +0500

    1.9. Объекты и классы

---
+++ src/Book.java
@@ -0,0 +1,31 @@
+public class Book {
+
+    private final String title;
+
+    private int publishDate;
+
+    private final Author author;
+    public Book (String title, int publishDate, Author author) {
+        this.title = title;
+        this.publishDate = publishDate;
+        this.author = author;
+    }
+
+    public void setPublishDate(int publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPublishDate() {
+        return publishDate;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,17 @@
 import java.util.Locale;
 public class Main {
 
-    public static void isLeapYearAndPrint(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println(year + " - високосный год");
-        }else System.out.println(year + " - не високосный год");
-    }
-
-    public static void checkAndPrintAppsVersion(int clientOS, int year) {
-        if (clientOS == 0 && year >= 2015) {
-            System.out.println("Установите новую версию IOS по ссылке ...");
-        } else if (clientOS == 0 && year < 2015) {
-            System.out.println("Установите облегчённую версию IOS по ссылке ...");
-        } else if (clientOS == 1 && year < 2015) {
-            System.out.println("Установите облегчённую версию Android по ссылке ...");
-        } else if (clientOS == 1 && year >= 2015) {
-            System.out.println("Установите новую версию Android по ссылке ...");
-        } else
-            System.out.println("Для вашей ОС нет приложения");
-
-    }
-
-    public static int calculateDeliveryDays(int distance) {
-        if (distance <= 20){
-            return 1;
-        } else if (distance <= 60) {
-            return 2;
-        } else if (distance <= 100) {
-            return 3;
-        } else {
-            throw new RuntimeException("Доставка не выполняется");
-        }
-    }
-
-
     public static void main(String[] args) {
-        int year = 2015;
-        isLeapYearAndPrint(year);
-        int clientOS = 0;
-        checkAndPrintAppsVersion(clientOS, year);
-        int deliveryDistance = 95;
-        System.out.println("количество дней на доставку - " + calculateDeliveryDays(deliveryDistance));
+
+        Author levTolstoy = new Author("Лев", "Толстой");
+        Author viktorPelevin = new Author("Виктор", "Пелевин");
+        Book warAndPeace = new Book("Война и мир", 1965, levTolstoy);
+        Book generationP = new Book("Дженерейшн Пи", 1999, viktorPelevin);
+
+        warAndPeace.setPublishDate(1867);
+
+        System.out.println (warAndPeace.getTitle() + " " + warAndPeace.getPublishDate() + " " + warAndPeace.getAuthor());
+        System.out.println (generationP.getTitle() + " " + generationP.getPublishDate() + " " + generationP.getAuthor());
 
 
     }
