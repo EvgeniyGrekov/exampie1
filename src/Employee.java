563603427251f3455744b605a54a933e98045862
Евгений <grekovqwe@gmail.com>
2022-08-26 01:15:53 +0500

    1.11. Курсовая работа 1. Базовая сложность.

---
+++ src/Employee.java
@@ -0,0 +1,60 @@
+public class Employee {
+
+    private int id;
+    private String fullName;
+    private int department;
+    private double salary;
+    private static int counter;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Id работника = " + id +
+                ", ФИО = '" + fullName + '\'' +
+                ", отдел = " + department +
+                ", ЗП = " + salary;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public static void setCounter(int counter) {
+        Employee.counter = counter;
+    }
+
+    public Employee(String fullName, int department, double salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        id = ++counter;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,141 @@
 import java.util.Locale;
 public class Main {
 
+    static Employee[] employees = new Employee[10];
+
     public static void main(String[] args) {
+        employees[0] = new Employee("Вася Пупкин", 1, 5000.0);
+        employees[1] = new Employee("Федя Васечкин", 2, 8000.0);
+        employees[2] = new Employee("Вася Федичкинн", 3, 9000.0);
+        employees[3] = new Employee("Петя Манукян", 4, 3000.0);
+        employees[4] = new Employee("Люся Кокаинова", 5, 7000.0);
+        employees[6] = new Employee("null", 1, 5000.0);
+        employees[8] = new Employee("Уй Алупкин", 1, 6000.0);
+        getAndCalkulateSalarySum();
 
-        Author levTolstoy = new Author("Лев", "Толстой");
-        Author viktorPelevin = new Author("Виктор", "Пелевин");
-        Book warAndPeace = new Book("Война и мир", 1965, levTolstoy);
-        Book warAndPeaceSecond = new Book("Война и мир", 1965, levTolstoy);
-        Book generationP = new Book("Дженерейшн Пи", 1999, viktorPelevin);
+        printAllEmployees();
 
-        warAndPeace.setPublishDate(1867);
+        System.out.println();
+        System.out.println("Сумма затрат на зарплаты в месяц = " + getAndCalkulateSalarySum());
+        System.out.println("Сотрудник с минимальной зарплатой - " + getEmployeeMinSalary());
+        System.out.println("Сотрудник с максимальной зарплатой - " + getEmployeeMaxSalary());
+        System.out.println("Средняя зарплата в месяц = " + getANDcALCULATEAverageSalary());
+        System.out.println();
 
-        System.out.println (warAndPeace);
-        System.out.println (generationP);
+        printFullNameAllEmployee();
 
-        System.out.println ();
+        System.out.println();
 
-        System.out.println(warAndPeace.equals(warAndPeaceSecond));
+        System.out.println("Всё ОК");
 
     }
 
+    public static void printAllEmployees() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+
+    }
+
+    public static double getAndCalkulateSalarySum() {
+        double sum = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                sum = sum + employee.getSalary();
+            }
+        }
+        return sum;
+    }
+
+    public static Employee getEmployeeMinSalary() {
+        double minSalary = -1;
+        Employee minSalaryEmpl = null;
+        int index = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                minSalary = employees[i].getSalary();
+                minSalaryEmpl = employees[i];
+                index = i;
+                break;
+            }
+        }
+        for (int i = index; i < employees.length; i++) {
+            if (employees[i] != null) {
+                if (minSalary > employees[i].getSalary()) {
+                    minSalary = employees[i].getSalary();
+                    minSalaryEmpl = employees[i];
+                }
+            }
+        }
+        return minSalaryEmpl;
+    }
+
+    public static Employee getEmployeeMaxSalary() {
+        double maxSalary = -1;
+        Employee maxSalaryEmpl = null;
+        int index = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                maxSalary = employees[i].getSalary();
+                maxSalaryEmpl = employees[i];
+                index = i;
+                break;
+            }
+        }
+        for (int i = index; i < employees.length; i++) {
+            if (employees[i] != null) {
+                if (maxSalary < employees[i].getSalary()) {
+                    maxSalary = employees[i].getSalary();
+                    maxSalaryEmpl = employees[i];
+                }
+            }
+        }
+        return maxSalaryEmpl;
+    }
+    public static double getANDcALCULATEAverageSalary(){
+        double sumSalaryAllEmpl = getAndCalkulateSalarySum();
+        if (employees.length != 0) {
+            return sumSalaryAllEmpl / employees.length;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void printFullNameAllEmployee(){
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee.getFullName());
+            }
+        }
+
+    }
 }
+
+
+    // Учёт сотрудников
+    // Автоматизация процессов бухгалтерии и отдела кадров
+    //
+    // Бухгалтерия и отдел кадров смогут узнавать следующую информацию:
+    // 1. Получить список всех сотрудников.
+    // 2. Посчитать сумму затрат на зарплаты.
+    // 3. Найти сотрудника с минимальной заплатой.
+    // 4. Найти сотрудника с максимальной зарплатой.
+    // 5. Подсчитать среднее значение зарплат.
+
+//        1. Создать класс Employee, который содержит информацию о Ф.И.О., отделе и зарплате сотрудника. Отделы для простоты должны быть названы от 1 до 5.
+//        2. Добавить статическую переменную-счетчик, которая будет отвечать за id.
+//        3. Добавить в класс Employee поле id, которое проставляется из счетчика, а затем счетчик увеличивает свое значение.
+//        4. Добавить возможность получать значения полей из Employee (геттеры) для всех полей.
+//        5. Добавить возможность устанавливать значения полей отдела и зарплаты (сеттеры).
+//        6. По умолчанию все поля должны передавать через конструктор (кроме id) и заполняться в нем (включая id, который нужно получить из счетчика).
+//        7. Создать внутри класса с методом main поле типа Employee[10], которое будет выполнять роль «хранилища» для записей о сотрудниках.
+//        8. Создать статические методы, которые будут взаимодействовать с массивом и предоставлять результат:
+//           a. Получить список всех сотрудников со всеми имеющимися по ним данными (вывести в консоль значения всех полей (toString)).
+//           b. Посчитать сумму затрат на зарплаты в месяц.
+//           c. Найти сотрудника с минимальной зарплатой.
+//           d. Найти сотрудника с максимальной зарплатой.
+//           e. Подсчитать среднее значение зарплат (можно использовать для этого метод из пункта b).
+//           f. Получить Ф. И. О. всех сотрудников (вывести в консоль).
+
+
+
